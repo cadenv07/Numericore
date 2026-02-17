77239eb7f8fd06223b58538313d45d4957222fa4
Caden Vize <cadenv07@iastate.edu>
2026-02-17 01:11:26 -0600

    feature: added main window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-#include <fmt/core.h>
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    fmt::print("vcpkg + CMake works {}\n", 123);
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW3" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800,600,"Numericore", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW3" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0,0,0,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
